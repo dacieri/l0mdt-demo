c939b41686f26ef2fd3ee644a3db58025540f03e
Davide Cieri <davide.cieri@cern.ch>
2024-09-27 15:22:14 +0200

    restyling

--- streamlit_app.py
+++ streamlit_app.py
@@ -28,9 +28,14 @@ events = get_events()  # This assumes the events are named event0, event1, etc.
 # Get the current event and its images
 current_event = events[st.session_state.folder_index]
 
-header_placeholder = st.empty()
-# Placeholder for image
+columns = st.columns(3, gap="small", vertical_alignment='bottom')
+with columns[0]:
+    header_placeholder = st.empty()
+    # Placeholder for image
+bar_placeholder = st.empty()
 image_placeholder = st.empty()
+
+
 
 # tnames = ["Step 0. Unfiltered Hits", "Step 1. Hits Filtered in Space and Time", "Step 3. Fit the Segment"]
 tnames = ["1. Ungefilterte Myon-Treffer", "2. Gefilterte Myon-Treffer", "3. Rekonstruierte Myon-Spur"]
@@ -47,23 +52,24 @@ current_tname = tnames[st.session_state.image_index]
 # List of button names for next image
 bnames = ["Filter the Hits in Time", "Filter the Hits in Space", "Identify the Hits on Segment", "Fit the Segment", "Next Event"]
 current_button_name = bnames[st.session_state.image_index]
+with columns[1]:
+    # Button with the name of the next image
+    # Start/Stop slideshow button
+    if st.button("Start/Stopp Slideshow", type="primary"):
+        st.session_state.slideshow_active = not st.session_state.slideshow_active
+with columns[2]:
+    if st.button(f"Nächste Kollision"):
+        # Move to the next image in the current folder
+        st.session_state.folder_index += 1
 
-# Button with the name of the next image
-if st.button(f"Nächste Kollision"):
-    # Move to the next image in the current folder
-    st.session_state.folder_index += 1
+        # If we've reached the end of the current folder's images
+        if st.session_state.folder_index >= len(events):
+            # Move to the next event folder and reset the image index
+            st.session_state.folder_index = 0
 
-    # If we've reached the end of the current folder's images
-    if st.session_state.folder_index >= len(events):
-        # Move to the next event folder and reset the image index
-        st.session_state.folder_index = 0
+        # Force rerun to update the UI with the new image immediately
+        st.rerun()
 
-    # Force rerun to update the UI with the new image immediately
-    st.rerun()
-
-# Start/Stop slideshow button
-if st.button("Start/Stopp Slideshow"):
-    st.session_state.slideshow_active = not st.session_state.slideshow_active
 
 
 # If slideshow is active, wait for 10 seconds and then move to the next event
@@ -76,15 +82,16 @@ with header_placeholder:
 with image_placeholder:
     # image = Image.open()
     with st.container():
-        # columns = st.columns(2, gap="medium", vertical_alignment='top')
-        for im in range(3):
-            # with columns[im % 2]:
-            st.subheader(tnames[im])
-            if im == 2:
-                st.image(current_event_path + "/mdt_3.png")
-            else:
-                st.image(current_event_path + "/mdt_%d.png" % im)
-        # image = Image.open(current_event_path + "/mdt_1.png")
+        imcol = st.columns(1)
+        with imcol[0]:
+            for im in range(3):
+                # with columns[im % 2]:
+                st.subheader(tnames[im])
+                if im == 2:
+                    st.image(current_event_path + "/mdt_3.png")
+                else:
+                    st.image(current_event_path + "/mdt_%d.png" % im)
+            # image = Image.open(current_event_path + "/mdt_1.png")
         # st.image(current_event_path + "/mdt_1.png")
         # # image = Image.open(current_event_path + "/mdt_2.png")
         # st.image(current_event_path + "/mdt_2.png")
@@ -92,13 +99,14 @@ with image_placeholder:
         # st.image(current_event_path + "/mdt_3.png")
         # # image = Image.open(current_event_path + "/mdt_4.png")
         # st.image(current_event_path + "/mdt_4.png")
-if st.session_state.slideshow_active:
-    my_bar = st.progress(0, text="Moving to next event...")
-    for percentage_complete in range(100):
-        time.sleep(0.1)
-        my_bar.progress(percentage_complete + 1, text="Moving to next event...")
-    # time.sleep(10)  # Wait for 10 seconds before moving to the next image
-    st.session_state.folder_index += 1
-    if st.session_state.folder_index >= len(events):
-        st.session_state.folder_index = 0
-    st.rerun()  # Refresh the page
+with bar_placeholder:
+    if st.session_state.slideshow_active:
+        my_bar = st.progress(0, text="Moving to next event...")
+        for percentage_complete in range(100):
+            time.sleep(0.1)
+            my_bar.progress(percentage_complete + 1, text="Moving to next event...")
+        # time.sleep(10)  # Wait for 10 seconds before moving to the next image
+        st.session_state.folder_index += 1
+        if st.session_state.folder_index >= len(events):
+            st.session_state.folder_index = 0
+        st.rerun()  # Refresh the page
